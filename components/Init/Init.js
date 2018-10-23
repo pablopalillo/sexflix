c60b8a36358a03809f8f50a20009d01b46324a50
pablopalillo <pablo.576@hotmail.com>
2018-10-23 18:27:05 -0500

    set color and main page

--- components/Init/Init.js
+++ components/Init/Init.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default class Init extends React.Component {
     render() {
         return (
           <View style={styles.container}>
-            <Text>Open up App.js to start working on your ass!</Text>
+            <View style={styles.topBar}>
+              <Text style={styles.text}>Sexflix</Text>
+            </View>
+
+            <ScrollView>
+              <Text style={styles.text}>Open up App.js to start working on your ass!</Text>
+            </ScrollView>
           </View>
         );
       }
@@ -14,9 +20,16 @@ export default class Init extends React.Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: '#141414',
     },
+    topBar:{
+      height: 50,
+      alignItems: 'center',
+      borderBottomColor: 'white',
+      borderBottomWidth: StyleSheet.hairlineWidth
+    },
+    text: {
+      color: "white"
+    }
   });
   
